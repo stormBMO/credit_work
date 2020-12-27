c52f8fb1feeea49ea79e4306be4aa950091ac7c9
stormBMO <andrey.chet76@gmail.com>
2020-12-27 19:33:03 +0300

    Added simple to-do

---
+++ js/todo.js
@@ -0,0 +1,22 @@
+// Function called while clicking add button 
+function add_item() { 
+
+     // Getting box and ul by selecting id; 
+     let item = document.getElementById("box"); 
+     let list_item = document.getElementById("list_item"); 
+     if(item.value != ""){ 
+          let make_li = document.createElement("LI"); 
+          make_li.appendChild(document.createTextNode(item.value)); 
+          list_item.appendChild(make_li); 
+          item.value = "";
+          make_li.onclick = function(){ 
+               this.parentNode.removeChild(this); 
+          } 
+     
+     } 
+     else{ 
+          alert("Add a value to item"); 
+     } 
+     
+     } 
+     
